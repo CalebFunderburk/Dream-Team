c4aeb1f8d039f7610d564655a50dbc8766923781
Caleb <calebfunderburk@icloud.com>
2022-09-22 14:54:24 -0400

    inquirer prompts questions finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // External requirments
 const inquirer = require('inquirer')
 const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
 // Empty array for team members
 teamArray = []
@@ -17,7 +19,7 @@ const initialPrompt = () => {
                 if (nameInput) {
                     return true
                 } else {
-                    console.log('Please enter the name of the Manager!')
+                    console.log('Please enter the Managers name!')
                     return false
                 }
             }
@@ -66,6 +68,7 @@ const initialPrompt = () => {
     .then(managerData => {
         const { name, id, email, officeNumber } = managerData
         const manager = new Manager (name, id, email, officeNumber)
+        // Put new object into team array
         teamArray.push(manager)
         console.log(`Welcome ${managerData.name}!`)
     })
@@ -73,6 +76,11 @@ const initialPrompt = () => {
 
 // Second prompt to figure what to do next
 const choicePrompt = () => {
+    console.log(`
+    ===========
+    |MAIN MENU|
+    ===========    
+    `)
     // Prompt for user to answer
     return inquirer.prompt([
         {
@@ -85,26 +93,115 @@ const choicePrompt = () => {
     // Conditional to reach the next desired step
     .then(choiceData => {
         if(choiceData.choice === 'Add Employee') {
-            console.log(choiceData.choice)
             employeePrompt()
         } else {
             console.log(choiceData.choice)
             // Generate html if 'Finish' was picked
+            showData()
         }
     })
 }
 
 // Emplyee creation prompt
 const employeePrompt = () => {
+    console.log(`
+    ==============
+    |NEW EMPLOYEE|
+    ==============
+    `)
     // Prompt for user to answer
-    return inquirer.prompt({
+    return inquirer.prompt([
         {
             type: 'list',
             name: 'role',
-            message: 'What is the employees current role?',
+            message: 'What is the Employees current role?',
             choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Employees name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the Employees name!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Employees ID?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the Employees ID!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Employees email?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the Managers email!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the Engineers GitHub username?',
+            when: (input) => input.role === 'Engineer',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the Managers email!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the Interns school name?',
+            when: (input) => input.role === 'Intern',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the Interns school!')
+                    return false
+                }
+            }
+        },
+    ])
+    .then(employeeData => {
+        let { name, id, email, role, github, school } = employeeData
+        let employee
+        if (role === 'Engineer') {
+            employee = new Engineer (name, id, email, github)
+            console.log(employee)
+        } else if (role === 'Intern') {
+            employee = new Intern (name, id, email, school)
+            console.log(employee)
         }
+        teamArray.push(employee)
+        choicePrompt()
     })
+}
+
+const showData = () => {
+    console.log(teamArray)
 }
 
 // App initialization
